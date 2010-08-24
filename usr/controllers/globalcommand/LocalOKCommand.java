4c8a3f161989bd01fdc842b3ef3416d9d673b752
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-08-24 21:03:38 +0000

    Local communication seems to be working now
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@149 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ usr/controllers/globalcommand/LocalOKCommand.java
@@ -0,0 +1,34 @@
+package usr.controllers.globalcommand;
+
+import usr.controllers.*;
+import usr.interactor.*;
+import java.nio.channels.SocketChannel;
+import usr.common.LocalHostInfo;
+
+public class LocalOKCommand extends AbstractCommand {
+    /**
+     * Construct a QuitCommand.
+     */
+    public LocalOKCommand() {
+        super(MCRP.OK_LOCAL_CONTROLLER.CMD, MCRP.OK_LOCAL_CONTROLLER.CODE, 
+          MCRP.ERROR.CODE);
+    }
+
+    /**
+     * Evaluate the Command.
+     */
+    public boolean evaluate(String req) {
+        String []args= req.split(" ");
+        if (args.length != 3) {
+            error ("Expected three arguments for LocalOKCommand");
+            return false;
+        }
+        String hostName= args[1];
+        int port= Integer.parseInt(args[2]);
+        LocalHostInfo lh= new LocalHostInfo(hostName, port);
+        success("Local OK received from "+args[1]+":"+args[2]);
+        controller.aliveMessage(lh);
+        return true;
+    }
+
+}
