ed4248709ec4b5c00ab030298cc0097574ab4873
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-09-30 15:11:46 +0000

    Add missing class file
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@265 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ usr/APcontroller/ConstructAPController.java
@@ -0,0 +1,27 @@
+package usr.APcontroller;
+
+
+import usr.router.RouterOptions;
+
+/** Wrapper to construct appropriate AP controller object */
+public class ConstructAPController
+{
+    public static APController constructAPController(RouterOptions o)
+    {
+        APController a= null;
+        String name= o.getAPControllerName();
+        if (name.equals("Random")) {
+            a = new RandomAPController(o);
+            return a;
+        }  
+        if (name.equals("") || name.equals("Null")) {
+            a = new NullAPController(o);
+            return a;  
+        }
+        
+        System.err.println("Unknown Access Point controller name "+name+
+          " using null controller");
+        a= new NullAPController(o);
+        return a;
+    }
+}
