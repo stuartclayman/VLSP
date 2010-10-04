cb085b652bb3704821937ac841f91d2218ee4c0e
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-10-04 15:19:39 +0000

    Added file for shut down router thread
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@275 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ usr/router/ShutDown.java
@@ -0,0 +1,58 @@
+package usr.router;
+
+import usr.protocol.MCRP;
+import usr.interactor.RouterInteractor;
+import usr.interactor.MCRPException;
+import usr.console.*;
+import usr.net.*;
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.nio.channels.SocketChannel;
+
+
+/**
+ * A EndLink ends connection between two links
+ */
+public class ShutDown extends ChannelResponder implements Runnable {
+    RouterController controller;
+    Request request;
+
+    /**
+     * Create a new connection.
+     * CREATE_CONNECTION ip_addr/port connection_weight - create a new network
+     * interface to a router on the address ip_addr/port with a 
+     * connection weight of connection_weight
+     */
+    public ShutDown(RouterController controller, Request request) {
+        this.controller = controller;
+        this.request = request;
+        setChannel(request.channel);
+    }
+
+    public void run() {
+        // process the request
+        System.out.println(leadin()+"Sending response code for shut down"+MCRP.SHUT_DOWN.CODE);
+        
+        respond(MCRP.SHUT_DOWN.CODE + " SHUTTING DOWN ROUTER");
+      
+        controller.shutDown();
+     
+    }
+
+    /**
+     * Create the String to print out before a message
+     */
+    String leadin() {
+        final String EL = "SD: ";
+
+        if (controller == null) {
+            return EL;
+        } else {
+            return controller.getName() + " " + EL;
+        }
+
+    }
+
+
+}
