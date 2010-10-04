0340455922289556b9ca825434c8937268521cce
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-10-04 17:26:22 +0000

    Tweaking debugging of shutdown code
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@297 83772449-cf8d-4b79-a1b1-a7e526131002

--- usr/router/AppSocketMux.java
+++ usr/router/AppSocketMux.java
@@ -197,7 +197,7 @@ public class AppSocketMux implements NetIF, Runnable {
     /**
      * Notify this thread.
      */
-    private synchronized void theEnd() {
+    private void theEnd() {
         System.out.println(leadin() + "theEnd");
         while (!ended()) {
             try {
