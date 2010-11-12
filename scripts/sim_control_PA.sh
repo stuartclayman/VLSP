350388de53f16d9a3ed681f7f9b0f0870e7d2aac
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-11-12 15:18:07 +0000

    Altered some scripts
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@439 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ scripts/sim_control_PA.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+OUTPUT=random_sim_PA
+POLICY=Random
+RIN=scripts/routerdummy.xml
+ROUT=scripts/routeroptions.xml
+PIN=scripts/probdummy_PA.xml
+POUT=scripts/probdists.xml
+AWK=gawk
+rm -f $OUTPUT
+for i in  0.1 0.2 0.5 1.0 2.0 5.0 10.0 20.0 50.0 100.0; do
+    for j in `seq 5`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
+    sed -e 's/xxx/'$i'/g' $PIN > $POUT
+    java usr.globalcontroller.GlobalController scripts/simcontrol.xml  > out
+    tail -50 summary.out | $AWK '{a+=$1; b+=$2; c+=$3;d+=$4;e+=$5;f+=$6;n++;}END{print a/n,b/n,c/n,d/n,e/n,f/n;}' >> $OUTPUT
+  done
+done
+POLICY=Pressure
+OUTPUT=pressure_sim_PA
+rm -f $OUTPUT
+for i in  0.1 0.2 0.5 1.0 2.0 5.0 10.0 20.0 50.0 100.0; do
+    for j in `seq 5`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
+    sed -e 's/xxx/'$i'/g' $PIN > $POUT
+    java usr.globalcontroller.GlobalController scripts/simcontrol.xml  > out
+    tail -50 summary.out | $AWK '{a+=$1; b+=$2; c+=$3;d+=$4;e+=$5;f+=$6;n++;}END{print a/n,b/n,c/n,d/n,e/n,f/n;}' >> $OUTPUT
+  done
+done
+POLICY=HotSpot
+OUTPUT=hotspot_sim_PA
+rm -f $OUTPUT
+for i in 0.1 0.2 0.5 1.0 2.0 5.0 10.0 20.0 50.0 100.0; do
+    for j in `seq 5`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
+    sed -e 's/xxx/'$i'/g' $PIN > $POUT
+    java usr.globalcontroller.GlobalController scripts/simcontrol.xml  > out
+    tail -50 summary.out | $AWK '{a+=$1; b+=$2; c+=$3;d+=$4;e+=$5;f+=$6;n++;}END{print a/n,b/n,c/n,d/n,e/n,f/n;}' >> $OUTPUT
+  done
+done
+
