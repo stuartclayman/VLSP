f9893b10dc72bf629a5176f56a3b2e82319f88c1
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-11-13 15:54:36 +0000

    Update run script
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@458 83772449-cf8d-4b79-a1b1-a7e526131002

--- scripts/clay_testbed_noPA.sh
+++ scripts/clay_testbed_noPA.sh
@@ -13,11 +13,12 @@ AWK=gawk
 rm -f $OUTPUT
 POLICY=Pressure
 OUTPUT=pressure_testbed_noPA
+SCHEDULE="5.0 10.0 15.0 20.0 25.0"
 
 
-for i in  7.5  12.5  17.5 22.5 27.5; do
+for i in $SCHEDULE; do
     for j in `seq 3`; do
-    echo -n $i " " >> $OUTPUT
+    echo -n $i " " $j >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
     sed -e 's/xxx/'$i'/g' $PIN > $POUT
     java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL  > out
@@ -28,7 +29,7 @@ done
 POLICY=HotSpot
 OUTPUT=hotspot_testbed_noPA
 rm -f $OUTPUT
-for i in 7.5  12.5  17.5 22.5 27.5 ; do
+for i in $SCHEDULE; do
     for j in `seq 3`; do
     echo -n $i " " >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
@@ -38,7 +39,7 @@ for i in 7.5  12.5  17.5 22.5 27.5 ; do
     tail -50 traffic.agg | $AWK '{a+=$1; b+=$2; c+=$3;d+=$4;e+=$5;f+=$6;n++;}END{printf("%g %g %g %g %g %g\n",a/n,b/n,c/n,d/n,e/n,f/n);}' >> $OUTPUT
   done
 done
-for i in  7.5  12.5  17.5 22.5 27.5; do
+for i in $SCHEDULE; do
     for j in `seq 3`; do
     echo -n $i " " >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
