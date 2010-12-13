ce33d7a195f41b48c46c8e824f7a471a81082247
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-12-13 17:25:41 +0000

    Updated script
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@565 83772449-cf8d-4b79-a1b1-a7e526131002

--- scripts/sim_time_noPA.sh
+++ scripts/sim_time_noPA.sh
@@ -6,31 +6,35 @@ RIN=scripts/routerdummy_time.xml
 ROUT=scripts/routeroptions_time.xml
 AWK=gawk
 
-SEQ="0.0 1.0 2.0 3.0"
+SEQ="0.0 1.0 10.0 100.0"
 REPS="10"
 
 OUTPUT=random_sim_time_noPA
 POLICY=Random
 rm -f $OUTPUT
+count= 0
 for i in  $SEQ; do
     for j in `seq $REPS`; do
-    echo -n $i "" >> $OUTPUT
+    echo -n $count $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN | sed -e 's/xxx/'$i'/g' > $ROUT
     java usr.globalcontroller.GlobalController $MASTERSCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+    count=`expr $count + 1` 
   done
 done
 POLICY=Pressure
 OUTPUT=pressure_sim_time_noPA
 rm -f $OUTPUT
+count= 0
 for i in $SEQ; do
     for j in `seq $REPS`; do
-    echo -n $i "" >> $OUTPUT
+    echo -n $count $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN | sed -e 's/xxx/'$i'/g' > $ROUT
     java usr.globalcontroller.GlobalController $MASTERSCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+        count=`expr $count + 1` 
   done
 done
 POLICY=HotSpot
@@ -38,11 +42,12 @@ OUTPUT=hotspot_sim_time_noPA
 rm -f $OUTPUT
 for i in $SEQ; do
     for j in `seq $REPS`; do
-    echo -n $i "" >> $OUTPUT
+    echo -n $count $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN | sed -e 's/xxx/'$i'/g' > $ROUT
     java usr.globalcontroller.GlobalController $MASTERSCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+    count=`expr $count + 1` 
   done
 done
 
