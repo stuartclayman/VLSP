205dbc53755244d288ccb7356d360f168ca33cd1
Stuart Clayman <s.clayman@ucl.ac.uk>
2010-12-15 02:07:35 +0000

    Update scripts
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@581 83772449-cf8d-4b79-a1b1-a7e526131002

--- scripts/clay_testbed_time_noPA.sh
+++ scripts/clay_testbed_time_noPA.sh
@@ -3,6 +3,10 @@ MASTERSCRIPT=scripts/testbed_time_noPA.xml
 
 RIN=scripts/routertestbeddummy_time.xml
 ROUT=scripts/routertestbed_time.xml
+CPVAR=/home/rclegg/code/userspacerouter/libs/monitoring-0.6.7.jar:/home/rclegg/code/userspacerouter/libs/timeindex-20101020.jar:/home/rclegg/code/userspacerouter/libs/aggregator-0.3.jar:/home/rclegg/code/userspacerouter
+CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/claycleanscript.sh
+
+
 AWK=gawk
 
 SEQ="0.0 0.1 1.0 10.0 100.0"
@@ -20,7 +24,8 @@ for i in  $SEQ; do
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
-  done
+    $CLEANSCRIPT
+    done
   count=`expr $count + 1` 
 done
 POLICY=Pressure
@@ -32,6 +37,7 @@ for i in $SEQ; do
     echo -n $count $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN | sed -e 's/xxx/'$i'/g' > $ROUT
     java -cp . usr.globalcontroller.GlobalController $MASTERSCRIPT  > out
+    $CLEANSCRIPT
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
@@ -47,6 +53,7 @@ for i in $SEQ; do
     echo -n $count $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN | sed -e 's/xxx/'$i'/g' > $ROUT
     java -cp . usr.globalcontroller.GlobalController $MASTERSCRIPT  > out
+    $CLEANSCRIPT
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
