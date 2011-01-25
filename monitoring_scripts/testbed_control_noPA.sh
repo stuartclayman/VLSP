f80f45fcf84c87eb8d075a6477d832b81c910360
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-01-25 17:47:29 +0000

    Fix scripts
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@590 83772449-cf8d-4b79-a1b1-a7e526131002

--- monitoring_scripts/testbed_control_noPA.sh
+++ monitoring_scripts/testbed_control_noPA.sh
@@ -5,6 +5,10 @@ ROUT=monitoring_scripts/testbed_routeroptions.xml
 CONTROL_SCRIPT=monitoring_scripts/testbed_control_noPA.xml
 AWK=gawk
 
+CPVAR=/home/rclegg/code/userspacerouter:/home/rclegg/code/userspacerouter/libs/monitoring-0.6.7.jar:/home/rclegg/code/userspacerouter/libs/timeindex-20101020.jar:/home/rclegg/code/userspacerouter/libs/aggregator-0.3.jar
+CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/claycleanscript.sh
+
+
 SEQ="0.01 0.02 0.05 0.1 0.2"
 ITER=5
 OUTPUT=random_testbed_noPA
@@ -14,9 +18,12 @@ for i in  $SEQ; do
     for j in `seq $ITER`; do
     echo -n $i "" >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
-    java usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{f
+or (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+    $CLEANSCRIPT
   done
 done
 POLICY=Pressure
@@ -26,9 +33,12 @@ for i in $SEQ; do
     for j in `seq $ITER`; do
     echo -n $i " " >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
-    java usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{f
+or (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+    $CLEANSCRIPT
   done
 done
 POLICY=HotSpot
@@ -38,9 +48,12 @@ for i in $SEQ; do
     for j in `seq $ITER`; do
     echo -n $i " " >> $OUTPUT
     sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
-    java usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
     tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{f
+or (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
     echo >> $OUTPUT
+    $CLEANSCRIPT
   done
 done
 
