557a2dee59eff3b79966e4670df3f89549137425
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-23 12:22:56 +0000

    Improved clean script
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@619 83772449-cf8d-4b79-a1b1-a7e526131002

--- scripts/claycleanscript.sh
+++ scripts/claycleanscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "killing java and deleting temp files"
-ssh rclegg@clayone -C "killall -9 java; rm /space/rclegg/testbed*; rm /tmp/info-*; rm /tmp/agg-*"
-ssh rclegg@claytwo -C "killall -9 java; rm /space/rclegg/testbed*; rm /tmp/info-*; rm /tmp/agg-*"
-ssh rclegg@claythree -C "killall -9 java; rm /space/rclegg/testbed*; rm /tmp/info-*; rm /tmp/agg-*"
+ssh rclegg@clayone -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm {} \; ; find /tmp -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@claytwo -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm {} \; ; find /tmp -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@claythree -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm {} \; ; find /tmp -name \"agg-*\" -exec rm {} \;"
