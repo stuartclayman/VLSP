fd268849dfb39a55ae26ce98d6761e8fa8854e91
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-23 18:57:52 +0000

    Quieten cleanup scripts
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@631 83772449-cf8d-4b79-a1b1-a7e526131002

--- scripts/claycleanscript.sh
+++ scripts/claycleanscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "killing java and deleting temp files"
-ssh rclegg@clayone -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@claytwo -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@claythree -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
+ssh rclegg@clayone -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@claytwo -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@claythree -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
--- scripts/gascleanscript.sh
+++ scripts/gascleanscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "killing java and deleting temp files"
-ssh rclegg@ethane -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@methane -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@butane -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@propane -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
-ssh rclegg@pentane -C "killall -9 java; rm -f /space/rclegg/testbed*; find /tmp -name \"info-*\" -exec rm -f {} \; ; find /tmp -name \"agg-*\" -exec rm -f {} \;"
+ssh rclegg@ethane -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@methane -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@butane -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@propane -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
+ssh rclegg@pentane -C "killall -9 java; rm /space/rclegg/testbed*; find /tmp -user rclegg -name \"info-*\" -exec rm {} \; ; find /tmp -user rclegg -name \"agg-*\" -exec rm {} \;"
