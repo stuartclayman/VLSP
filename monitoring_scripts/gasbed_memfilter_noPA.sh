4d3dad187dd4d4e1f09d2500b5448c2925aa2cda
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-23 18:23:33 +0000

    Add scripts for gas testbed
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@626 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ monitoring_scripts/gasbed_memfilter_noPA.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+RIN=monitoring_scripts/gasbed_memfilter_dummy_noPA.xml
+ROUT=monitoring_scripts/gasbed_routeroptions.xml
+CONTROL_SCRIPT=monitoring_scripts/gasbed_memfilter_noPA.xml
+AWK=gawk
+
+CPVAR=/home/rclegg/code/userspacerouter:/home/rclegg/code/userspacerouter/libs/monitoring-0.6.7.jar:/home/rclegg/code/userspacerouter/libs/timeindex-20101020.jar:/home/rclegg/code/userspacerouter/libs/aggregator-0.3.jar
+CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/gascleanscript.sh
+
+
+SEQ="0.02 0.05 0.1"
+ITER=5
+OUTPUT=testbed_nofilter_noPA
+FILTER=always
+rm -f $OUTPUT
+for i in  $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i "" >> $OUTPUT
+    sed -e 's/yyy/'$FILTER'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+FILTER=2%
+OUTPUT=testbed_2perfilter_noPA
+rm -f $OUTPUT
+for i in $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$FILTER'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+FILTER=5%
+OUTPUT=testbed_5perfilter_noPA
+rm -f $OUTPUT
+for i in $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$FILTER'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+FILTER=10%
+OUTPUT=testbed_10perfilter_noPA
+rm -f $OUTPUT
+for i in $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$FILTER'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+
+
