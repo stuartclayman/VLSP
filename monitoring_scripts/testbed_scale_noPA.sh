d2be80258b4c76173ee22f413df2b9ec96f66a41
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-27 21:41:54 +0000

    More scripts
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@640 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ monitoring_scripts/testbed_scale_noPA.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+RIN=monitoring_scripts/testbed_scale_dummy_noPA.xml
+ROUT=monitoring_scripts/testbed_router_scale_options.xml
+PIN=monitoring_scripts/testbed_scale_probdummy.xml
+POUT=monitoring_scripts/testbed_scale_prob.xml
+CONTROL_SCRIPT=monitoring_scripts/testbed_scale_noPA.xml
+AWK=gawk
+
+CPVAR=/home/rclegg/code/userspacerouter:/home/rclegg/code/userspacerouter/libs/monitoring-0.6.7.jar:/home/rclegg/code/userspacerouter/libs/timeindex-20101020.jar:/home/rclegg/code/userspacerouter/libs/aggregator-0.3.jar
+CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/claycleanscript.sh
+
+
+SEQ="20.0 40.0 60.0 80.0"
+ITER=5
+OUTPUT=testbed_pressure_scale_noPA
+POLICY=Pressure
+rm -f $OUTPUT
+for i in  $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i "" >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
+    sed -e 's/xxx/'$i'/g' $PIN > $POUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
