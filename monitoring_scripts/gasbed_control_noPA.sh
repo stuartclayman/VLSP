784aee964265454856be6c6f2431aaf5a75d5857
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-28 16:01:30 +0000

    Update
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@650 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ monitoring_scripts/gasbed_control_noPA.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+RIN=monitoring_scripts/gasbed_router_dummy_noPA.xml
+ROUT=monitoring_scripts/gasbed_routeroptions.xml
+CONTROL_SCRIPT=monitoring_scripts/gasbed_control_noPA.xml
+AWK=gawk
+
+CPVAR=/home/rclegg/code/userspacerouter:/home/rclegg/code/userspacerouter/libs/monitoring-0.6.7.jar:/home/rclegg/code/userspacerouter/libs/timeindex-20101020.jar:/home/rclegg/code/userspacerouter/libs/aggregator-0.3.jar
+CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/gascleanscript.sh
+
+
+SEQ="0.02 0.05 0.1"
+ITER=5
+OUTPUT=random_gasbed_noPA
+POLICY=Random
+rm -f $OUTPUT
+for i in  $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i "" >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+POLICY=Pressure
+OUTPUT=pressure_gasbed_noPA
+rm -f $OUTPUT
+for i in $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+POLICY=HotSpot
+OUTPUT=hotspot_gasbed_noPA
+rm -f $OUTPUT
+for i in $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i " " >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN |  sed -e 's/xxx/'$i'/g' > $ROUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+
