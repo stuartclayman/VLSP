fe01700c9016aeef131827ae1a41702d2332bdd3
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-03-30 14:25:17 +0000

    newscript
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@656 83772449-cf8d-4b79-a1b1-a7e526131002

--- monitoring_scripts/testbed_scale_noPA.sh
+++ monitoring_scripts/testbed_scale_noPA.sh
@@ -13,8 +13,8 @@ CLEANSCRIPT=/home/rclegg/code/userspacerouter/scripts/claycleanscript.sh
 
 SEQ="20.0 40.0 60.0 80.0"
 ITER=5
-OUTPUT=testbed_pressure_scale_noPA
-POLICY=Pressure
+OUTPUT=testbed_random_scale_noPA
+POLICY=Random
 rm -f $OUTPUT
 for i in  $SEQ; do
     for j in `seq $ITER`; do
@@ -28,3 +28,19 @@ for i in  $SEQ; do
     $CLEANSCRIPT
   done
 done
+OUTPUT=testbed_hotspot_scale_noPA
+POLICY=HotSpot
+rm -f $OUTPUT
+for i in  $SEQ; do
+    for j in `seq $ITER`; do
+    echo -n $i "" >> $OUTPUT
+    sed -e 's/yyy/'$POLICY'/g' $RIN > $ROUT
+    sed -e 's/xxx/'$i'/g' $PIN > $POUT
+    java -cp $CPVAR usr.globalcontroller.GlobalController $CONTROL_SCRIPT  > out
+    tail -50 summary.out | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    tail -50 traffic.agg | $AWK '{for (i=1; i <= NF; i++) {a[i]+= $i} n++;}END{for (i=1; i <= NF; i++) printf("%g ",a[i]/n)}' >> $OUTPUT
+    echo >> $OUTPUT
+    $CLEANSCRIPT
+  done
+done
+
