a40d29a88dac23cc66470b3ca9e1a316519339a8
Stuart Clayman <s.clayman@ucl.ac.uk>
2011-10-19 13:13:43 +0000

    Added some files that were left out of the last checkin.
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@736 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ usr/router/AppListProbe.java
@@ -0,0 +1,188 @@
+// AppListProbe.java
+
+package usr.router;
+
+import usr.router.*;
+import usr.applications.ApplicationHandle;
+import eu.reservoir.monitoring.core.*;
+import eu.reservoir.monitoring.core.table.*;
+import eu.reservoir.monitoring.appl.datarate.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A probe that talks to a Router can collects the stats
+ * for each executing App.
+ */
+public class AppListProbe extends RouterProbe implements Probe {
+    // The TableHeader for the table of stats
+    TableHeader statsHeader;
+
+    // Save table, so we only send different ones
+    DefaultTable savedT = null;
+
+    /**
+     * Construct a AppListProbe
+     */
+    public AppListProbe(RouterController cont) {
+        setController(cont);
+
+        // set probe name
+        setName(cont.getName()+".appList");
+        // set data rate
+        setDataRate(new EveryNSeconds(10));
+
+        // Define the header. Has:
+        // Name
+        // ThreadName
+        // ClassName
+        // State
+        statsHeader = new DefaultTableHeader()
+                      .add("ID", ProbeAttributeType.INTEGER)
+                      .add("StartTime", ProbeAttributeType.LONG)
+                      .add("State", ProbeAttributeType.STRING)
+                      .add("ClassName", ProbeAttributeType.STRING)
+                      .add("Args", ProbeAttributeType.STRING)
+                      .add("Name", ProbeAttributeType.STRING)
+                      ;
+
+                      //add("ThreadName", ProbeAttributeType.STRING);
+
+
+        // setup the probe attributes
+        // The router name
+        // The table of stats
+        addProbeAttribute(new DefaultProbeAttribute(0, "RouterName", ProbeAttributeType.STRING, "name"));
+        addProbeAttribute(new TableProbeAttribute(1, "Data", statsHeader));
+
+    }
+
+
+    /**
+     * Collect a measurement.
+     */
+    public ProbeMeasurement collect() {
+        //System.out.println("AppListProbe: collect()");
+
+        // get list of apps
+        Collection<ApplicationHandle> appList = getController().appList();
+
+        if (appList == null || appList.size() == 0) {
+            // no apps to report
+            return null;
+
+        } else {
+
+            try {
+
+                // collate measurement values
+                ArrayList<ProbeValue> list = new ArrayList<ProbeValue>();
+
+                // add router name
+                list.add(new DefaultProbeValue(0, getController().getName()));
+
+                // now allocate a table
+                DefaultTable statsTable = new DefaultTable();
+                statsTable.defineTable(statsHeader);
+
+
+                // visit each App
+                for (ApplicationHandle ah : appList) {
+                    // create a row for ApplicationHandle data
+                    TableRow appHRow = new DefaultTableRow();
+
+                    // ID
+                    appHRow.add(new DefaultTableValue(ah.getID()));
+
+                    // StartTime
+                    appHRow.add(new DefaultTableValue(ah.getStartTime()));
+
+                    // State
+                    appHRow.add(new DefaultTableValue(ah.getState().toString()));
+
+                    // ClassName
+                    appHRow.add(new DefaultTableValue(ah.getApplication().getClass().getName()));
+
+                    // Args
+                    appHRow.add(new DefaultTableValue(Arrays.asList(ah.getArgs()).toString()));
+
+                    // Name
+                    appHRow.add(new DefaultTableValue(ah.getName()));
+
+
+                    // ThreadName
+                    //appHRow.add(new DefaultTableValue(ah.getThreadName()));
+
+                    // add this row to the table
+                    statsTable.addRow(appHRow);
+
+
+                }
+
+                list.add(new DefaultProbeValue(1, statsTable));
+
+                // if the tables are the same, don not send a new measurement
+                if (tablesEqual(savedT, statsTable)) {
+                    // nothing to send
+                    return null;
+                } else {
+                    // set the type to be: AppList
+                    ProducerMeasurement lastestM = new ProducerMeasurement(this, list, "AppList");
+                    savedT = statsTable;
+                    return lastestM;
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
+    /**
+     * Are tables equal
+     */
+    private boolean tablesEqual(Table t1, Table t2) {
+        // check all the rows
+
+        if (t1 == null || t2 == null) {
+            return false;
+        } else {
+            // get sizes
+            int t1Rows = t1.getRowCount();
+            int t2Rows = t2.getRowCount();
+
+            if (t1Rows != t2Rows) {
+                // different size - must be different
+                return false;
+            } else {
+                // same size - check rows
+                for (int r=0; r < t1Rows; r++) {
+                    // see if the rows are equal
+                    TableRow t1Row = t1.getRow(r);
+                    TableRow t2Row = t2.getRow(r);
+
+                    int size = t1Row.size();
+
+                    for (int e=0; e < size; e++) {
+                        TableValue t1V = t1Row.get(e);
+                        TableValue t2V = t2Row.get(e);
+                    
+                        if (! t1V.getValue().equals(t2V.getValue())) {
+                            // a value is different - therefore table must be different
+                        return false;
+                        }
+                    }
+                }
+
+                // all the rows are the same
+                return true;
+            
+            }
+        }
+    }
+}
