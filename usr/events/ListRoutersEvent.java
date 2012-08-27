6c6e3d3b428ec1ab3689df9be72411c1df6fac1d
Stuart Clayman <s.clayman@ucl.ac.uk>
2012-08-27 23:40:14 +0000

    Update
    git-svn-id: https://svn.ee.ucl.ac.uk/repos/jefferson/userspacerouter@995 83772449-cf8d-4b79-a1b1-a7e526131002

---
+++ usr/events/ListRoutersEvent.java
@@ -0,0 +1,44 @@
+package usr.events;
+
+import usr.logging.*;
+import java.lang.*;
+import usr.globalcontroller.*;
+import usr.engine.*;
+import us.monoid.json.*;
+import usr.common.*;
+
+/** Class represents a global controller event*/
+public class ListRoutersEvent extends Event
+{
+
+public ListRoutersEvent (long time){
+    engine_ = null;
+}
+
+
+public String toString(){
+    String str;
+
+    str = "ListRouters: " + time_;
+    return str;
+}
+
+
+public JSONObject execute(GlobalController gc) 
+        throws InstantiationException {
+    JSONObject jsobj = new JSONObject();
+    JSONArray array = new JSONArray();
+    try {
+        jsobj.put("success",true);
+        for (BasicRouterInfo info : gc.getAllRouterInfo())
+            array.put(info.getId());
+        jsobj.put("type", "router");
+        jsobj.put("list", array);
+    } catch (JSONException je) {
+        Logger.getLogger("log").logln(
+            USR.ERROR,
+            "JSONException in StartLinkEvent should not occur");
+    }
+    return jsobj;
+}
+}
